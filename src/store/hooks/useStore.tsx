285658e9af960afef932b752919e1d0e97ee3fcb
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-10 21:39:31 -0300

    less events fired

--- src/store/hooks/useStore.tsx
+++ src/store/hooks/useStore.tsx
@@ -1,17 +1,31 @@
-import { useContext } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useContext, useMemo } from 'react'
 import { StateContext } from '../StoreProvider'
 import { State } from '../initialState';
 
 export const useStore = <K extends keyof State>(property: K, types: any): [State[K], any, any] => {
   const state = useContext(StateContext)
-  
-  let error: any = {}
-  let loading: any = {}
 
-  Object.keys(types).forEach(type => {
-    error[type] = state.error[type] || ''
-    loading[type] = state.loading[type] || false
-  })
+  const requiredState = useMemo(() => {
+    return state[property]
+  }, [state[property]])
 
-  return [ state[property], error, loading ]
+  const error = useMemo(() => {
+    const error: any = {}
+    Object.keys(types).forEach(type => error[type] = state.error[type])
+    return error
+  },
+  [...Object.keys(types).map(type => state.error[type])]
+  )
+
+  const loading = useMemo(() => {
+    const loading: any = {}
+    Object.keys(types).forEach(type => loading[type] = state.loading[type])
+    return loading
+  },
+    [...Object.keys(types).map(type => state.loading[type])]
+  )
+
+
+  return [ requiredState, error, loading ]
 }
