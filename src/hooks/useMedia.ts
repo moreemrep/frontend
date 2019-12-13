7520382a9b9b3004c321204261a86a4a0ac2f27b
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-12 22:35:58 -0300

    wip responsivo

---
+++ src/hooks/useMedia.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+export function useMedia(query: string) {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches)
+
+  useEffect(() => {
+    const media = window.matchMedia(query)
+
+    if (media.matches !== matches) {
+      setMatches(media.matches)
+    }
+
+    const listener = () => setMatches(media.matches)
+    media.addListener(listener)
+
+    return () => media.removeListener(listener)
+  }, [query])
+
+  return matches
+}
--- src/pages/ProcurarRepublica.tsx
+++ src/pages/ProcurarRepublica.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useRepublicaActions } from '../actions/useRepublicaActions'
 import { useRepublicaStore } from '../store/reducers/republicas-reducer'
 import { Tipo } from '../generated/graphql'
+import { useMedia } from '../hooks/useMedia'
 
 export const ProcurarRepublica: React.FC = () => {
   const [republicas, error, loading] = useRepublicaStore()
   const { fetchRepublicas } = useRepublicaActions()
+  const small = useMedia('(max-width: 400px)')
 
   const [filtro] = useState({ localizacao: [1, 2], tipo: Tipo.Mista })
 
@@ -14,6 +16,22 @@ export const ProcurarRepublica: React.FC = () => {
   }, [])
 
   if (loading.FETCH_REPUBLICAS) return <div>loading...</div>
+
+  if (small) {
+    return (
+      <div>
+        {error.FETCH_REPUBLICAS}
+        small
+        {republicas.map(republica => (
+          <div key={republica.nome}>
+            <p>
+              {republica.nome}({republica.distancia} metros)
+            </p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 
   return (
     <div>
