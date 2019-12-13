c0beb40f2c4a8db774a62fa2f9f9976d700a8b3a
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-12 22:35:42 -0300

    particles

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
+import Particles from 'react-particles-js'
 
 export const LandingPage: React.FC = () => {
-  return <div>Bem vindo</div>
+  return (
+    <div>
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              width: 0.9,
+              color: '#0F0',
+              opacity: 1
+            },
+            number: {
+              value: 100,
+              max: 1000
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                color: '#fff',
+                width: 0.6
+              }
+            },
+            move: {
+              straight: false,
+              bounce: true,
+              random: true,
+              speed: 5
+            }
+          }
+        }}
+        style={{
+          width: '100%',
+          backgroundColor: '#7B26CD'
+        }}
+      />
+    </div>
+  )
 }
