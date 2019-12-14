8d081da942eae108a35fb6f345d0b88efb4e0595
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-13 21:58:00 -0300

    form centralizado

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './style.css'
 
 export const LandingPage: React.FC = () => {
   return (
-    <div>
+    <div className="formLanding">
       <input type="text" name="inp1" />
       <button> Procurar </button>
     </div>
