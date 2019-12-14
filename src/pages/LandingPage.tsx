b69605be1036ec5e20c4213c0ecab1c26caed361
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-14 10:51:45 -0300

    add map

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
+import Map from 'pigeon-maps'
 
 const styles = StyleSheet.create({
   form: {
@@ -15,6 +16,7 @@ export const LandingPage: React.FC = () => {
     <div className={css(styles.form)}>
       <input type="text" name="inp1" />
       <button> Procurar </button>
+      <Map center={[50.879, 44.6997]} zoom={12} width={600} height={400} />
     </div>
   )
 }
