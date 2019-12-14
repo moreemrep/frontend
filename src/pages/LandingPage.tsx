ec37d1f9eb98e33900924eb021546c46e943706d
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-14 10:01:19 -0300

    css in js with aphrodite

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './style.css'
+import { StyleSheet, css } from 'aphrodite'
+
+const styles = StyleSheet.create({
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+    minHeight: '80%',
+    justifyContent: 'center'
+  }
+})
 
 export const LandingPage: React.FC = () => {
   return (
-    <div className="formLanding">
+    <div className={css(styles.form)}>
       <input type="text" name="inp1" />
       <button> Procurar </button>
     </div>
