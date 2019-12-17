acdbbc9edb08aecda31e50d8d0e835f96a9bffbc
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-17 17:21:33 -0300

    login e esqueci senha

--- src/view/Pages/Login.tsx
+++ src/view/Pages/Login.tsx
@@ -1,58 +1,106 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuthActions } from 'src/actions/useAuthActions';
 import { useAuthStore } from 'src/store/reducers/auth-reducer';
-import { Form, Button, Row, Col, InputGroup } from 'react-bootstrap';
+import { Form, Button, Col, InputGroup } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import { useHistory } from 'react-router';
+import { object, string } from 'yup';
 
 const Login: React.FC = () => {
   const { login, forgotPassword } = useAuthActions();
   const [, error, loading] = useAuthStore();
+  const history = useHistory();
 
-  if (loading.LOGIN || loading.FORGOT_PASSWORD) return <div>loading...</div>;
+  const { handleChange, handleSubmit, values, errors, touched, handleBlur, setFieldError } = useFormik({
+    initialValues: {
+      email: '',
+      senha: ''
+    },
+    validationSchema: object().shape({
+      email: string()
+        .required('Email inválido')
+        .email('Email inválido'),
+      senha: string()
+        .required('Senha obrigatória')
+        .min(6, 'minimo 6 caracteres')
+    }),
+    onSubmit: async (values, helpers) => {
+      if (await login(values)) {
+        history.push('editar');
+      }
+    }
+  });
+
+  useEffect(() => {
+    if (!error.FORGOT_PASSWORD) return;
+
+    setFieldError('email', error.FORGOT_PASSWORD);
+  }, [error.FORGOT_PASSWORD]);
+
+  useEffect(() => {
+    if (!error.LOGIN) return;
+
+    if (error.LOGIN.includes('password')) {
+      setFieldError('senha', error.LOGIN);
+    } else {
+      setFieldError('email', error.LOGIN);
+    }
+  }, [error.LOGIN]);
 
   return (
-    <div>
-      {error.LOGIN}
-      {error.FORGOT_PASSWORD}
-      <Form className="formEnter">
-        <Form.Group as={Col} md="6" controlId="validationFormikUsername">
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text id="inputGroupPrepend">Email:</InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control
-              type="text"
-              placeholder="Digite o Email"
-              aria-describedby="inputGroupPrepend"
-              // name="username"
-              // onChange={handleChange}
-              // isInvalid={!!errors.username}
-            />
-            {/* <Form.Control.Feedback type="invalid">{errors.username}</Form.Control.Feedback> */}
-          </InputGroup>
-        </Form.Group>
+    <Form className="formEnter">
+      <Form.Group as={Col} md="6" controlId="validationFormikUsername">
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text id="inputGroupPrepend">Email:</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type="text"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder="Digite o Email"
+            aria-describedby="inputGroupPrepend"
+            isInvalid={touched.email && !!errors.email}
+          />
+          <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+        </InputGroup>
+      </Form.Group>
 
-        <Form.Group as={Col} md="6" controlId="validationFormikSenha">
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text id="inputGroupPrepend">Senha:</InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control
-              type="password"
-              placeholder="Digite a Senha"
-              aria-describedby="inputGroupPrepend"
-              // onChange={handleChange}
-            />
-          </InputGroup>
-        </Form.Group>
+      <Form.Group as={Col} md="6" controlId="validationFormikSenha">
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text id="inputGroupPrepend">Senha:</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type="password"
+            name="senha"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            placeholder="Digite a Senha"
+            aria-describedby="inputGroupPrepend"
+            isInvalid={touched.senha && !!errors.senha}
+          />
+          <Form.Control.Feedback type="invalid">{errors.senha}</Form.Control.Feedback>
+        </InputGroup>
+      </Form.Group>
 
-        <Button variant="outline-primary" size="lg">
+      {!loading.LOGIN && (
+        <Button variant="outline-primary" size="lg" onClick={() => handleSubmit()}>
           Entrar
         </Button>
-        <Button className="BtEsqueceuSh" variant="outline-secondary" size="sm">
+      )}
+      {!loading.FORGOT_PASSWORD && (
+        <Button
+          onClick={() => forgotPassword(values.email)}
+          className="BtEsqueceuSh"
+          variant="outline-secondary"
+          size="sm"
+        >
           Esqueceu a senha
         </Button>
-      </Form>
-    </div>
+      )}
+    </Form>
   );
 };
 
