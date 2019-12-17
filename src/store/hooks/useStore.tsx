2e6cad3f8c02c43faba11b1fc2f436beca978b66
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-16 21:02:06 -0300

    action status with types

--- src/store/hooks/useStore.tsx
+++ src/store/hooks/useStore.tsx
@@ -1,25 +1,44 @@
-import { useContext, useMemo } from 'react'
-import { StateContext } from '../StoreProvider'
-import { State } from '../initialState'
+import { useContext, useMemo } from 'react';
+import { StateContext } from '../StoreProvider';
+import { State } from '../initialState';
 
-export const useStore = <K extends keyof State>(property: K, types: any): [State[K], any, any] => {
-  const state = useContext(StateContext)
+export function useStore<K extends keyof State>(property: K): State[K] {
+  const state = useContext(StateContext);
 
   const requiredState = useMemo(() => {
-    return state[property]
-  }, [state[property]])
+    return state[property];
+  }, [state[property]]);
+
+  return requiredState;
+}
+
+function useLoading<T>(types: any): T {
+  const state = useContext(StateContext);
+
+  const loading: T = useMemo(() => {
+    const loading: any = {};
+    Object.keys(types).forEach(type => (loading[type] = state.loading[type]));
+    return loading;
+  }, [...Object.keys(types).map(type => state.loading[type])]);
+
+  return loading;
+}
+
+function useError<T>(types: any): T {
+  const state = useContext(StateContext);
 
   const error = useMemo(() => {
-    const error: any = {}
-    Object.keys(types).forEach(type => (error[type] = state.error[type]))
-    return error
-  }, [...Object.keys(types).map(type => state.error[type])])
+    const error: any = {};
+    Object.keys(types).forEach(type => (error[type] = state.error[type]));
+    return error;
+  }, [...Object.keys(types).map(type => state.error[type])]);
 
-  const loading = useMemo(() => {
-    const loading: any = {}
-    Object.keys(types).forEach(type => (loading[type] = state.loading[type]))
-    return loading
-  }, [...Object.keys(types).map(type => state.loading[type])])
+  return error;
+}
 
-  return [requiredState, error, loading]
+export function useStatus<T>(types: any): [T, T] {
+  const error = useError<T>(types);
+  const loading = useLoading<T>(types);
+
+  return [error, loading];
 }
--- src/store/reducers/republicas-reducer.tsx
+++ src/store/reducers/republicas-reducer.tsx
@@ -1,32 +1,39 @@
-import { useDispatch } from '../hooks/useDispatch'
-import { Action } from '../StoreProvider'
-import { useStore } from '../hooks/useStore'
-import { RepublicaPayload } from '../../generated/graphql'
+import { useDispatch } from '../hooks/useDispatch';
+import { Action } from '../StoreProvider';
+import { useStore, useStatus } from '../hooks/useStore';
+import { RepublicaPayload } from '../../generated/graphql';
 
-export const initialState: Array<RepublicaPayload> = []
+export const initialState: Array<RepublicaPayload> = [];
 
 const types = {
   FETCH_REPUBLICAS: 'FETCH_REPUBLICAS'
+};
+
+interface Status {
+  FETCH_REPUBLICAS: boolean;
 }
 
 export function republicaReducer(state: Array<RepublicaPayload>, action: Action) {
   switch (action.type) {
     case 'FETCH_REPUBLICAS_SUCCESS':
-      return action.payload
+      return action.payload;
 
     default:
-      return state
+      return state;
   }
 }
 
 export function useRepublicaDispatch() {
-  const fetch = useDispatch<Array<RepublicaPayload>>(types.FETCH_REPUBLICAS)
+  const fetch = useDispatch<Array<RepublicaPayload>>(types.FETCH_REPUBLICAS);
 
   return {
     fetch
-  }
+  };
 }
 
-export function useRepublicaStore() {
-  return useStore('republicas', types)
+export function useRepublicaStore(): [RepublicaPayload[], Status, Status] {
+  const republicas = useStore('republicas');
+  const [error, loading] = useStatus(types);
+
+  return [republicas, error, loading];
 }
