ae4120bface2756afbd66c2a772a083369365fdf
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-17 22:54:36 -0300

    test login responsivo

--- src/view/Pages/Login.tsx
+++ src/view/Pages/Login.tsx
@@ -5,13 +5,61 @@ import { Form, Button, Col, InputGroup, Container, Row } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import { useHistory } from 'react-router';
 import { object, string } from 'yup';
+import { StyleSheet, css, CSSProperties, StyleSheetStatic, StyleDeclaration } from 'aphrodite';
+import { useDimensions } from 'src/hooks/useDimensions';
+import { useBreakpoints } from 'src/hooks/useBreakpoints';
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'transparent',
+    borderColor: 'green',
+    color: 'white',
+    paddingTop: 25,
+    paddingBottom: 25
+  },
+  error: {
+    borderColor: 'red'
+  }
+});
 
 const Login: React.FC = () => {
   const { login, forgotPassword } = useAuthActions();
   const [, error, loading] = useAuthStore();
   const history = useHistory();
+  const { height, width } = useDimensions();
+  const { medium, large } = useBreakpoints();
 
-  const { handleChange, handleSubmit, values, errors, touched, handleBlur, setFieldError } = useFormik({
+  const Styles = {
+    inputContainer: {
+      width: width * 0.9
+    },
+    input: {
+      fontSize: 25,
+      paddingTop: 40,
+      paddingBottom: 40
+    }
+  };
+
+  if (medium) {
+    Styles.inputContainer.width = width * 0.7;
+    Styles.input.fontSize = 25;
+  } else if (large) {
+    Styles.inputContainer.width = width * 0.6;
+    Styles.input.fontSize = 30;
+  }
+
+  const responsiveStyles = StyleSheet.create({
+    ...Styles,
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: height * 0.9
+    }
+  });
+
+  const { handleChange, handleSubmit, values, errors, touched, handleBlur, setFieldError, validateForm } = useFormik({
     initialValues: {
       email: '',
       senha: ''
@@ -47,19 +95,26 @@ const Login: React.FC = () => {
     }
   }, [error.LOGIN]);
 
+  const handleForgotPassword = async () => {
+    const errors = await validateForm(values);
+    if (errors.email) {
+      alert(errors.email);
+    } else {
+      forgotPassword(values.email);
+    }
+  };
+
   return (
-    <Form className="formEnter">
+    <Form className={css(responsiveStyles.container)}>
       <Form.Group as={Row} md="6" controlId="validationFormikEmail">
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroupPrepend">Email:</InputGroup.Text>
-          </InputGroup.Prepend>
+        <InputGroup className={css(responsiveStyles.inputContainer)}>
           <Form.Control
             type="text"
             name="email"
+            className={css(styles.input, responsiveStyles.input)}
             onChange={handleChange}
             onBlur={handleBlur}
-            placeholder="Digite o Email"
+            placeholder="Email"
             aria-describedby="inputGroupPrepend"
             isInvalid={touched.email && !!errors.email}
           />
@@ -67,17 +122,15 @@ const Login: React.FC = () => {
         </InputGroup>
       </Form.Group>
 
-      <Form.Group as={Row} md="6" controlId="validationFormikSenha">
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroupPrepend">Senha:</InputGroup.Text>
-          </InputGroup.Prepend>
+      <Form.Group as={Row} controlId="validationFormikSenha">
+        <InputGroup className={css(responsiveStyles.inputContainer)}>
           <Form.Control
             type="password"
             name="senha"
+            className={css(styles.input, responsiveStyles.input)}
             onChange={handleChange}
             onBlur={handleBlur}
-            placeholder="Digite a Senha"
+            placeholder="Senha"
             aria-describedby="inputGroupPrepend"
             isInvalid={touched.senha && !!errors.senha}
           />
@@ -85,23 +138,23 @@ const Login: React.FC = () => {
         </InputGroup>
       </Form.Group>
 
-      <Container>
+      <Form.Group>
         <Button disabled={loading.LOGIN} variant="primary" size="lg" onClick={() => handleSubmit()}>
           {loading.LOGIN ? 'Carregando' : 'Entrar'}
         </Button>
-      </Container>
+      </Form.Group>
 
-      <Container>
+      <Form.Group>
         <Button
           disabled={loading.FORGOT_PASSWORD}
-          onClick={() => forgotPassword(values.email)}
+          onClick={handleForgotPassword}
           variant="outline-secondary"
           size="sm"
           className="BtEsqueceuSh"
         >
-          {loading.FORGOT_PASSWORD ? 'Carregando' : 'Esqueceu a senha'}
+          {loading.FORGOT_PASSWORD ? 'Carregando' : 'Esqueci minha senha'}
         </Button>
-      </Container>
+      </Form.Group>
     </Form>
   );
 };
